618a33036fa9de5589522b09601c4ff7b3dafdc5
MatiasBurecovics <111394291+MatiasBurecovics@users.noreply.github.com>
2023-06-02 12:07:52 -0300

    Update personajeController.js

--- src/controllers/personajeController.js
+++ src/controllers/personajeController.js
@@ -5,6 +5,7 @@ import { Authenticate } from '../common/jwt.strategy.js';
 const router = Router();
 const personajeService = new PersonajeService();
 
+
 router.get('', Authenticate , async (req, res) => {
   console.log(`This is a get operation`);
   
@@ -13,7 +14,7 @@ router.get('', Authenticate , async (req, res) => {
   return res.status(200).json(personajes);
 });
 
-router.get('', Authenticate, async (req, res) => {
+router.get('/:id', Authenticate, async (req, res) => {
   console.log(`Request URL Param: ${req.params.id}`);
   console.log(`This is a get operation`);
 
@@ -34,18 +35,18 @@ router.put('/:id',Authenticate,  async (req, res) => {
   console.log(`Request URL Param: ${req.params.id}`);
   console.log(`This is a put operation`);
 
-  const pizza = await pizzaService.updatePizzaById(req.body);
+  const updatePersonaje = await personajeService.updatePersonajeById(req.body);
 
-  return res.status(200).json(pizza);
+  return res.status(200).json(updatePersonaje);
 });
 
 router.delete('/:id',Authenticate,  async (req, res) => {
   console.log(`Request URL Param: ${req.params.id}`);
   console.log(`This is a delete operation`);
 
-  const pizza = await pizzaService.deletePizzaById(req.params.id);
+  const eliminarPersonaje = await personajeService.deletePersonajeById(req.params.id);
 
-  return res.status(200).json(pizza);
+  return res.status(200).json(eliminarPersonaje);
 });
 
 export default router;
